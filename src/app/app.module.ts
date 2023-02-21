b32500a9479876d54a3fdd6bcc8e49e0cccab33e
yevgeny <yevgeny.shtraiher@highcon.net>
2023-02-21 17:09:19 +0200

    basic content projection example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ZippyMultislotComponent } from './zippy-multislot/zippy-multislot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ZippyMultislotComponent
   ],
   imports: [
     BrowserModule
